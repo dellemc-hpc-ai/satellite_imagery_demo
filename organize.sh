cd0b1270abd75ba968193ccd62c44a88436049c7
Rakshith Vasudev <rakshith_vasudev@dellteam.com>
2019-08-12 13:03:18 -0500

    updating docs

--- organize.sh
+++ organize.sh
@@ -1,7 +1,7 @@
 # this script organizes the files to be present in the structure that the notebook expects.
 # This script should be present inside the dataset folder.
 #!/bin/bash
-tar -xvf train_images.tgz 
+tar -xvf train_images.tgz -C sample_train_images
 cd sample_train_images
 mv train_images/* .
 cp sample_train_images/1038.tif ../
