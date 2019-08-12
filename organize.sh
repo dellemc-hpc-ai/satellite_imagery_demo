b9d753901586d79737c896cca659372aed5a01d1
Rakshith Vasudev <rakshith_vasudev@dellteam.com>
2019-08-12 16:19:15 -0500

    fixed organize

--- organize.sh
+++ organize.sh
@@ -3,12 +3,36 @@
 #!/bin/bash
 
 mkdir sample_train_images
-tar -xvf val_images.tgz -C sample_train_images
-cd sample_train_images
-mv val_images/* .
-cp 1038.tif ../
-cp 1075.tif ../ 
-cp 108.tif ../
-cd ../
+tar -xvf train_images.tgz -C sample_train_images
+mv sample_train_images/train_images/* sample_train_images/
+
+# backup files before deleting
+cp sample_train_images/102.tif .
+cp sample_train_images/1036.tif .
+cp sample_train_images/1037.tif .
+cp sample_train_images/1042.tif .
+
+
+rm -f sample_train_images/*.tif
+
+# get the backup
+mv 102.tif sample_train_images/
+mv 1036.tif sample_train_images/
+mv 1037.tif sample_train_images/
+mv 1042.tif sample_train_images/
+
+
+mkdir sample_val_images
+
+# extract validation images
+tar -xvf val_images.tgz -C sample_val_images
+
+# backup files before deleting
+cp sample_val_images/val_images/1038.tif .
+cp sample_val_images/val_images/1075.tif .
+cp sample_val_images/val_images/108.tif .
+
+rm -rf sample_val_images
+
 tar -xvf train_labels.tgz
 
